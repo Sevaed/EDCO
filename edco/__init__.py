fb14f44129d32ee2b4af970e08f1b5df0520c120
Sevaed <seva.romanovsky@gmail.com>
2025-08-10 03:54:40 +0300

    Made system of publication easier, now i need only push new tag with new version

--- edco/__init__.py
+++ edco/__init__.py
@@ -1,2 +1,6 @@
-__version__ = "1.2"
-
+try:
+    from importlib.metadata import version as _pkg_version
+    __version__ = _pkg_version("edco")
+except Exception:
+    # когда пакет ещё не установлен (локальные запуски), пусть будет "0.0.0"
+    __version__ = "0.0.0"
