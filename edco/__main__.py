3166e04e7a6c4dfd034a53c21320f6914a3aa84e
Sevaed <seva.romanovsky@gmail.com>
2026-05-25 14:05:39 +0300

    now working

--- edco/__main__.py
+++ edco/__main__.py
@@ -3,13 +3,24 @@ from . import tui
 from . import data
 import sys
 
-def main(args):
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
     if len(args) == 0:
         tui.run_tui()
     elif args[0] == "--_list-names":
         cmd.list_names()
     else:
-        commands = {"-p":cmd.path, "-c":cmd.cat, "-a":cmd.add_element, "-n":cmd.names,"-h":cmd.help, "--help":cmd.help, "-d":cmd.del_smth}
+        commands = {
+            "-p": cmd.path,
+            "-c": cmd.cat,
+            "-a": cmd.add_element,
+            "-n": cmd.names,
+            "-h": cmd.help,
+            "--help": cmd.help,
+            "-d": cmd.del_smth,
+        }
         if args[0] in commands:
             commands[args[0]](*args[1:])
         elif args[0] in data.get_data():
@@ -18,6 +29,7 @@ def main(args):
             cmd.flag_not_found(args[0])
         else:
             cmd.name_not_found()
+
 
 if __name__ == "__main__":
     args = sys.argv[1:]
